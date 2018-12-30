665dc146eddd9e61524dc4a775a00ce053f65a54
悦欣 张 <zhangyuexin121@live.cn>
2018-12-30 11:31:54 +0800

    Select 6 Hour Data

--- ASP.NET/WeatherStation/WebPage/Controllers/WeatherController.cs
+++ ASP.NET/WeatherStation/WebPage/Controllers/WeatherController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Utility;
+using Utility.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -21,10 +22,49 @@ namespace WebPage.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            var data = _context.Weathers.OrderByDescending(x => x.DateTime)
-                .Take(6)
-                .OrderBy(x => x.DateTime)
-                .ToList();
+            List<Weather> data = new List<Weather>();
+
+            #region Select 6 Hour Data
+            var groups = _context.Weathers.GroupBy(x => x.DateTime.Date)
+                .OrderByDescending(x => x.Key)
+                .Take(2)
+                .Select(g => g.Select(x => x).ToList()).ToList();
+
+            var today = groups[0].GroupBy(x => x.DateTime.Hour)
+                .OrderByDescending(x => x.Key)
+                .Select(g => g.Select(x => x).ToList()).ToList();
+
+            int count = today.Count;
+            if (count >= 6)
+            {
+                var temp = today.Take(6);
+
+                foreach (var item in temp)
+                {
+                    data.Add(item[0]);
+                }
+            }
+            else
+            {
+                var yesterday = groups[1].GroupBy(x => x.DateTime.Hour)
+                    .OrderByDescending(x => x.Key)
+                    .Select(g => g.Select(x => x).ToList()).ToList();
+
+                var temp = yesterday.Take(6 - count);
+
+                foreach (var item in today)
+                {
+                    data.Add(item[0]);
+                }
+
+                foreach (var item in temp)
+                {
+                    data.Add(item[0]);
+                }
+            }
+
+            data.Reverse();
+            #endregion
 
             return View(data);
         }
