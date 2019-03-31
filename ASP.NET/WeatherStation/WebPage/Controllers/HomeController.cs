8f03441a01ce574841fe9f442ebf87dc1fba6083
悦欣 张 <zhangyuexin121@live.cn>
2019-03-31 19:51:00 +0800

    bootstrap 4

--- ASP.NET/WeatherStation/WebPage/Controllers/HomeController.cs
+++ ASP.NET/WeatherStation/WebPage/Controllers/HomeController.cs
@@ -41,5 +41,17 @@ namespace WebPage.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+            );
+
+            return LocalRedirect(returnUrl);
+        }
     }
 }
--- ASP.NET/WeatherStation/WebPage/Controllers/WeatherController.cs
+++ ASP.NET/WeatherStation/WebPage/Controllers/WeatherController.cs
@@ -76,17 +76,5 @@ namespace WebPage.Controllers
 
             return View();
         }
-
-        [HttpPost]
-        public IActionResult SetLanguage(string culture, string returnUrl)
-        {
-            Response.Cookies.Append(
-                CookieRequestCultureProvider.DefaultCookieName,
-                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
-                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
-            );
-
-            return LocalRedirect(returnUrl);
-        }
     }
 }
